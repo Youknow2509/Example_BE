21026c59beb17b91783c3340eb99f1a39493c0b9
vinh-vps-5662210 <lytranvinh.wprk@gmail.com>
2024-06-22 16:38:45 +0000

    [Upgrade]: CHange title, tag, decription swagger

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,18 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     const config = new DocumentBuilder()
-        .setTitle('Cats example')
-        .setDescription('The cats API description')
+        .setTitle('V')
         .setVersion('1.0')
-        .addTag('cats')
+        .addTag('V')
+        .setDescription(`
+            Contact:
+            - Mail: lytranvinh.work@gmail.com 
+            - Github: https://github.com/Youknow2509 
+            Source: 
+            - Github: https://github.com/Youknow2509/Example_BE
+        `)
         .build();
+        
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
 
