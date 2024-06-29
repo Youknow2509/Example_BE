e8f4d59e7b8bf953b6807f40a18cec7a75bc4234
vinh-vps-5662210 <lytranvinh.wprk@gmail.com>
2024-06-29 18:29:47 +0000

    [Upgrade]: import auth in module root

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
-import { AuthModule } from './db/google-sheet/auth/auth.module';
+import { AuthModule } from './auth/auth.module';
 import { GoogleSheetModule } from './db/google-sheet/google-sheet.module';
 import { CreateTokenModule } from './db/google-sheet/create-token/create-token.module';
 
 @Module({
     imports: [
+        AuthModule,
         UserModule, 
         CreateTokenModule
     ],
