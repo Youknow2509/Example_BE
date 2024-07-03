5c7325d6ffdcc49a9e4080cf9863236055aafae1
vinh-vps-5662210 <lytranvinh.wprk@gmail.com>
2024-07-03 15:58:13 +0000

    [Upgrade]: Add role handle router

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -11,8 +11,10 @@ import {
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User, CreateUser } from './dto';
-import { AuthGuard } from '../auth/auth.guard';
-import { Roles } from 'src/decorator/roles.decorator';
+import { AuthGuardRole } from '../auth/auth.guard.role';
+import { Roles } from 'src/decorator';
+import { Role } from '../enum/';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller('user')
 export class UserController {
@@ -20,20 +22,27 @@ export class UserController {
     constructor(private readonly userService: UserService) {}
 
     // Get all user
-    @Roles('admin')
-    @UseGuards(AuthGuard)
+    @Roles(Role.VIEW)
+    @UseGuards(AuthGuardRole)
+    @ApiBearerAuth()
     @Get()
     getAll(): any {
         return this.userService.getUsers();
     }
 
     // Add user
+    @Roles('user')
+    @UseGuards(AuthGuardRole)
+    @ApiBearerAuth()
     @Post()
     create(@Body() user: CreateUser): any {
         return this.userService.appendDataUser(user);
     }
     
     // Upgrade user
+    @Roles('admin')
+    @UseGuards(AuthGuardRole)
+    @ApiBearerAuth()
     @Put('/upgrade')
     upgrade(@Body() user: User): any {
         user.updated_at = new Date();
@@ -41,18 +50,27 @@ export class UserController {
     }
 
     // Find user
+    @Roles('admin')
+    @UseGuards(AuthGuardRole)
+    @ApiBearerAuth()
     @Get(':id')
     find(@Param('id', ParseIntPipe) id: number): any {
         return this.userService.findUser(id);
     }
 
     // Delete user
+    @Roles('admin')
+    @UseGuards(AuthGuardRole)
+    @ApiBearerAuth()
     @Delete(':id')
     delete(@Param('id', ParseIntPipe) id: number): any {
         return this.userService.deleteUser(id);
     }
 
     // Restore user
+    @Roles('admin')
+    @UseGuards(AuthGuardRole)
+    @ApiBearerAuth()
     @Put(':id/restore')
     restore(@Param('id', ParseIntPipe) id: number): any {
         return this.userService.restoreUser(id);
