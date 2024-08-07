4b939c2e1d899ed0b5f4805f4e6937f96c9b94e9
Ly Tran Vinh <88392742+Youknow2509@users.noreply.github.com>
2024-08-07 17:38:22 +0000

    [Upgrade]: add listen event newMessage, replyMessage

--- src/chat_app/chat_app.gateway.ts
+++ src/chat_app/chat_app.gateway.ts
@@ -14,28 +14,31 @@ import { AuthServiceChatApp } from './chat_app.guard';
 export class ChatAppGateway
     implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit
 {
+    // Variables
     private connectedClients: Map<string, Socket> = new Map();
-
-    constructor(private readonly authService: AuthServiceChatApp) {}
-
     @WebSocketServer() server: Server;
     private logger: Logger = new Logger('ChatGateway');
+    // Constructor
+    constructor(private readonly authService: AuthServiceChatApp) {}
 
-    @SubscribeMessage('message')
-    handleMessage(
-        client: Socket,
-        payload: { sender: string; message: string },
-    ): void {
-        this.server.emit('message', payload);
-    }
-
+    /**
+     * Deloy OnGatewayInit is called when the server is initialized
+     * @param server
+     */
     afterInit(server: Server) {
         this.logger.log('WebSocket server initialized');
     }
 
+    /**
+     * Handle connection is called when a client connects to the server
+     * Use jwt authentication to authenticate the client
+     * @param client
+     */
     handleConnection(client: Socket) {
         try {
-            const token = this.authService.extractTokenFromHandshake(client.handshake.query);
+            const token = this.authService.extractTokenFromHandshake(
+                client.handshake.query,
+            );
             const user = this.authService.verifyToken(token);
             if (!user) {
                 client.disconnect();
@@ -43,14 +46,49 @@ export class ChatAppGateway
             }
             this.connectedClients.set(client.id, client);
             this.logger.log(`Client connected: ${client.id}`);
+            this.connectedClients.set(client.id, client);
         } catch (error) {
             this.logger.error(`Client connection error: ${error.message}`);
             client.disconnect();
         }
     }
 
+    /**
+     * Handle disconnection is called when a client disconnects from the server
+     * @param client
+     */
     handleDisconnect(client: Socket) {
         this.connectedClients.delete(client.id);
         this.logger.log(`Client disconnected: ${client.id}`);
     }
+
+    /**
+     * Event handler for new messages
+     * @param client 
+     * @param payload 
+     */
+    @SubscribeMessage('newMessage')
+    handleMessage(
+        client: Socket,
+        payload: { sender: string; message: string },
+    ): void {
+        this.server.emit('message', payload);
+    }
+
+    /**
+     * Event handler for replying to messages
+     * @param client 
+     * @param payload 
+     */
+    @SubscribeMessage('replyMessage')
+    handleReply(
+        client: Socket,
+        payload: { sender: string; recipient: string; message: string },
+    ): void {
+        const recipientClient = this.connectedClients.get(payload.recipient);
+        if (recipientClient) {
+            recipientClient.emit('message', payload);
+        }
+    }
+    // TODO: Add more event handlers
 }
